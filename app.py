9303cc109c9e66504cdd1693a4302a5f28db82e2
OTinin7 <166668454+OTinin7@users.noreply.github.com>
2025-07-25 13:20:15 -0300

    v 1.2

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ st.markdown(
     }
 
     .stMarkdown, .stTextInput, .stSelectbox, label, span, p, div {
-        color: black !important;
+        color: blue !important;
     }
 
     div[data-baseweb="select"] > div {
@@ -55,12 +55,12 @@ nome = st.text_input("Seu nome")
 escolha = {
     "Não tenho interesse": None,
     "Bolo de Pote-R$7,00": "bolodepote.jpeg",
-    "Brigadeiros- RS 0,80/unid(15g) || RS 1,30/unid(25g)": "brigadeiros.jpeg",
+    "Brigadeiros- R$ 0,80/unid(15g) || R$ 1,30/unid(25g)": "brigadeiros.jpeg",
     "Fatia Gourmet - R$ 7,50": "fatias.jpeg",
-    "Torta (1,5KG-RS60,00)(2,5KG-RS95,00)": "torta.jpeg",
+    "Torta (1,5KG-R$60,00)(2,5KG-R$95,00)": "torta.jpeg",
     "Torta Vitrine (2,0KG-R$120,00)": "bolovit.jpeg",
     "Kit festa (A partir de R$140,00)": "kitfesta.jpeg",
-    "Bolo Decorado (RS65/kg -sem topo)": "bolodecorado.jpeg"
+    "Bolo Decorado (R$65/kg -sem topo)": "bolodecorado.jpeg"
 }
 
 st.subheader("Escolha o que você tem interesse:")
@@ -73,8 +73,6 @@ for nome_bolo, imagem_bolo in escolha.items():
             f"<p style='text-align: center; font-size: 20px; color: black; font-weight: bold;'>{nome_bolo}</p>",
             unsafe_allow_html=True
         )
-
-
 
 
 
